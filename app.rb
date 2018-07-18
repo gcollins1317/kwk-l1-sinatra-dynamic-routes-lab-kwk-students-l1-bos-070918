612a32a67bda3493b13d8b97875cbd5b3954f159
gcollins1317 <gracecollins1000@gmail.com>
2018-07-18 13:34:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,6 +24,6 @@ get '/say/:number/:phrase' do
   "#{@everything}"
 end 
 get '/say/:word1/:word2/:word3/:word4/:word5' do 
-  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
+  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
 end 
 end
